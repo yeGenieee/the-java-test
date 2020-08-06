ddc7b353d12b214c7904823e30334aeb8f523875
yeGenieee <cyj95428@gmail.com>
2020-08-06 21:04:51 +0900

    feat: Add limit property & Add testcode for limit

--- src/test/java/com/yegenieee/inflearnthejavatest/StudyTest.java
+++ src/test/java/com/yegenieee/inflearnthejavatest/StudyTest.java
@@ -2,16 +2,26 @@ package com.yegenieee.inflearnthejavatest;
 
 import org.junit.jupiter.api.*;
 
-import static org.junit.jupiter.api.Assertions.*;
-@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
-class StudyTest {
+import java.lang.reflect.Executable;
+import java.util.function.Supplier;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.*;
+
+class StudyTest {
     @Test
     @DisplayName("스터디 만들기")
     void create() {
-        Study study = new Study();
+        Study study = new Study(-10);
         assertNotNull(study);
-        assertEquals(StudyStatus.DRAFT, study.getStatus(), "스터디를 처음 만들면 상태값이 DRAFT여야 한다.");
+        assertEquals(StudyStatus.DRAFT, study.getStatus(), new Supplier<String>() {
+            @Override
+            public String get() {
+                return "스터디를 처음 만들면 상태값이 DRAFT여야 한다.";
+            }
+        });
+        assertTrue(1 < 2);
+        assertTrue(study.getLimit() > 0, "스터디 최대 참석 가능 인원은 0보다 커야 한다.");
     }
 
     @Test
